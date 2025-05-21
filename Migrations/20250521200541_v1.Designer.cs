70b079aff3e7e2c294024b1e26deb777ed1e4f36
isaquezs <caracktergames@gmail.com>
2025-05-21 17:09:48 -0300

    Inicializando pela primeira vez

--- Migrations/20250521200541_v1.Designer.cs
+++ Migrations/20250521200541_v1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ong.Data;
 
@@ -11,9 +12,11 @@ using Ong.Data;
 namespace Ong.Migrations
 {
     [DbContext(typeof(DbOng))]
-    partial class DbOngModelSnapshot : ModelSnapshot
+    [Migration("20250521200541_v1")]
+    partial class v1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
